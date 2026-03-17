698cc20fff16dd18e7b87e1cd048401810a66d54
1x0x7 <seoulcandlelong@gmail.com>
2026-03-17 13:55:30 +0900

    45p 자기소개 하기

---
+++ src/OpenChallenge01/MyInfoApp.java
@@ -0,0 +1,9 @@
+package OpenChallenge01;
+
+public class MyInfoApp {
+    public static void main(String[] args){
+        System.out.println("저의 이름은 황기태(Kitae Hwang)입니다.");
+        System.out.println("나이는 20살이고,");
+        System.out.println("학과는 컴퓨터 공학과입니다.");
+    }
+}
